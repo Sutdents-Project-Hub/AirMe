fda667e4819024d3daa06d780afa244a05aaab6a
Felix <lmx0901260128@gmail.com>
2026-07-13 18:47:09 +0800

    fix(web): 修正紀錄頁導覽與相依性

--- apps/client/src/components/app-tabs.web.tsx
+++ apps/client/src/components/app-tabs.web.tsx
@@ -24,8 +24,8 @@ export default function AppTabs() {
           <TabTrigger name="home" href="/" asChild>
             <TabButton>Home</TabButton>
           </TabTrigger>
-          <TabTrigger name="explore" href="/explore" asChild>
-            <TabButton>Explore</TabButton>
+          <TabTrigger name="history" href="/history" asChild>
+            <TabButton>紀錄</TabButton>
           </TabTrigger>
         </CustomTabList>
       </TabList>
